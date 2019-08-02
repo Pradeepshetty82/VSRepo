4352330b336282aa5c70afe7defb0b388bf4f556
Pradeepshetty82 <53590861+Pradeepshetty82@users.noreply.github.com>
2019-08-02 15:25:19 +0530

    Update Program.cs

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -7,6 +7,7 @@ namespace App1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine( " Hi Sundeep");
         }
     }
 }
